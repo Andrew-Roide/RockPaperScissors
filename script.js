408bdb3d0e1bfc16455c668571a6f46af4d2f639
Andrew Roide <andrew.roide@hotmail.com>
2022-06-30 16:34:23 -0700

    Updated Pseudocode

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 // Allow the playerSelection to input their selection of either rock, paper, scissors
 // Make playerSelection case-insensitive
 // Let the computerPlay randomly select either rock, paper, scissors
-// If playerSelection input is equal to rock while computerPlay is equal to scissors, "You won! Rock beats Scissors"; Save win info for player
-// If playerSelection input is equal to rock while computerPlay is equal to paper, "You lost! Paper beats Rock"; Save win info for computer
-// If playerSelection input is equal to scissors while computerPlay is equal to rock, "You lost! Rock beats Scissors"; Save win info for computer
-// If playerSelection input is equal to scissors while computerPlay is equal to paper, "You won! Scissors beat Paper"; Save win info for player
-// If playerSelection input is equal to paper while computerPlay is equal to rock, "You won! Paper beats Rock"; Save win info for player
-// If playerSelection input is equal to paper while computerPlay is equal to scissors, "You lost! Scissors beat Paper"; Save win info for computer
-// If playerSelection input is equal to computerPlay "Tie game, Play again!"; Save win info both player and computer
+// If playerSelection input is equal to rock while computerPlay is equal to scissors, "You won! Rock beats Scissors"; Save win info for playerScore
+// If playerSelection input is equal to rock while computerPlay is equal to paper, "You lost! Paper beats Rock"; Save win info for computerScore
+// If playerSelection input is equal to scissors while computerPlay is equal to rock, "You lost! Rock beats Scissors"; Save win info for computerScore
+// If playerSelection input is equal to scissors while computerPlay is equal to paper, "You won! Scissors beat Paper"; Save win info for playerScore
+// If playerSelection input is equal to paper while computerPlay is equal to rock, "You won! Paper beats Rock"; Save win info for playerScore
+// If playerSelection input is equal to paper while computerPlay is equal to scissors, "You lost! Scissors beat Paper"; Save win info for computerScore
+// If playerSelection input is equal to computerPlay "Tie game, Play again!"; Save win info both playerScore and computerScore
 // Replay the game 5 
 // if playerScore is less than computerScore, "The computer won!"
 // if playerScore is more than computerScore, "The player won!"
