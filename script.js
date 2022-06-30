572b1dab3fbe0d13e14166cf68aa5bf0945b8092
Andrew Roide <andrew.roide@hotmail.com>
2022-06-30 16:12:22 -0700

    Added pseudocode to problem solve

---
+++ script.js
@@ -0,0 +1,11 @@
+// Allow the playerSelection to input their selection of either rock, paper, scissors
+// Make playerSelection case-insensitive
+// Let the computerPlay randomly select either rock, paper, scissors
+// If playerSelection input is equal to rock while computerPlay is equal to scissors, "You won! Rock beats Scissors"
+// If playerSelection input is equal to rock while computerPlay is equal to paper, "You lost! Paper beats Rock"
+// If playerSelection input is equal to scissors while computerPlay is equal to rock, "You lost! Rock beats Scissors"
+// If playerSelection input is equal to scissors while computerPlay is equal to paper, "You won! Scissors beat Paper"
+// If playerSelection input is equal to paper while computerPlay is equal to rock, "You won! Paper beats Rock"
+// If playerSelection input is equal to paper while computerPlay is equal to scissors, "You lost! Scissors beat Paper"
+// If playerSelection input is equal to computerPlay "Tie game, Play again!" 
+// 
