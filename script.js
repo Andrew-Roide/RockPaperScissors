16f9eb682282686315889d2bcfae3f0476ddccf5
Andrew Roide <andrew.roide@hotmail.com>
2022-07-05 12:02:12 -0700

    Created random selector from array for computer

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-// Allow the playerSelection to input their selection of either rock, paper, scissors
-// Make playerSelection case-insensitive
-// Let the computerPlay randomly select either rock, paper, scissors
+// Allow the playerSelection to input their selection of either rock, paper, scissors ~~~~done~~~~
+// Make playerSelection case-insensitive ~~~~done~~~~
+// Let the computerPlay randomly select either rock, paper, scissors ~~~~done~~~~
 // If playerSelection input is equal to rock while computerPlay is equal to scissors, "You won! Rock beats Scissors"; Save win info for playerScore
 // If playerSelection input is equal to rock while computerPlay is equal to paper, "You lost! Paper beats Rock"; Save win info for computerScore
 // If playerSelection input is equal to scissors while computerPlay is equal to rock, "You lost! Rock beats Scissors"; Save win info for computerScore
@@ -12,3 +12,15 @@
 // if playerScore is less than computerScore, "The computer won!"
 // if playerScore is more than computerScore, "The player won!"
 // if playerScore is equal to computerScore, "Tie game. Play another 5 rounds"
+
+let playerInput =  prompt("Choose Rock, Paper, or Scissors!");
+let playerSelection = playerInput.toLowerCase();
+const computerPlay = ['Rock','Paper','Scissors'];
+
+function computerSelection(){
+    let options = computerPlay.length;
+    let randomSelection = Math.floor(Math.random() * options);
+    let randomComputerSelection = computerPlay[randomSelection];
+    
+    return randomComputerSelection;
+}
