5f2d50d8493491ce7dc0becd70c243cb1c6cb63e
Andrew Roide <andrew.roide@hotmail.com>
2022-08-26 11:36:44 -0700

    sets player score-- bug : does not count first game

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 var choiceRock = "Rock";
 var choicePaper = "Paper";
 var choiceScissors = "Scissors";
+var playerOneScore = 0;
+var playerTwoScore = 0;
 
 function computerSelection() {
     let computerPlay = [choiceRock, choicePaper, choiceScissors];
@@ -17,11 +19,13 @@ function playRound(playerSelection) {
         (playerSelection === choiceScissors && computersMove === choicePaper) ||
         (playerSelection === choicePaper && computersMove === choiceRock)) {
         document.querySelector("h2").innerHTML = `Player 1 wins! ${playerSelection.toUpperCase()} beats ${computersMove.toUpperCase()} `;
+        document.querySelector(".p1-score").textContent = playerOneScore++;
     } else if (
         (playerSelection === choiceRock && computersMove === choicePaper) ||
         (playerSelection === choiceScissors && computersMove === choiceRock) ||
         (playerSelection === choicePaper && computersMove === choiceScissors)) {
         document.querySelector("h2").innerHTML = `Player 2 wins! ${computersMove.toUpperCase()} beats ${playerSelection.toUpperCase()} `;
+        document.querySelector(".p2-score").textContent = playerOneScore++;
     } else if (playerSelection === computersMove) {
         document.querySelector("h2").innerHTML = `Draw Game! ${playerSelection.toUpperCase()} vs ${computersMove.toUpperCase()} `;
 }
