7fe4ee68fa50c1c399fc23d6ce084f4c3770b2b2
Andrew Roide <andrew.roide@hotmail.com>
2022-08-27 11:20:51 -0700

    removed button fucntion

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 var choiceRock = "Rock";
 var choicePaper = "Paper";
 var choiceScissors = "Scissors";
-var playerOneScore = 1;
-var playerTwoScore = 1;
+var playerOneScore = 0;
+var playerTwoScore = 0;
 
+// random number computer will generate
 function computerSelection() {
     let computerPlay = [choiceRock, choicePaper, choiceScissors];
     let randomSelection = Math.floor(Math.random() * computerPlay.length);
@@ -12,6 +13,7 @@ function computerSelection() {
     return randomComputerSelection;
 }
 
+//plays a single round and produces round winner
 function playRound(playerSelection) {
     let computersMove = computerSelection();
         if (
@@ -19,18 +21,20 @@ function playRound(playerSelection) {
         (playerSelection === choiceScissors && computersMove === choicePaper) ||
         (playerSelection === choicePaper && computersMove === choiceRock)) {
         document.querySelector("h2").innerHTML = `Player 1 wins! ${playerSelection.toUpperCase()} beats ${computersMove.toUpperCase()} `;
-        document.querySelector(".p1-score").textContent = playerOneScore++;
+        document.querySelector(".p1-score").textContent = (playerOneScore++) + 1;
     } else if (
         (playerSelection === choiceRock && computersMove === choicePaper) ||
         (playerSelection === choiceScissors && computersMove === choiceRock) ||
         (playerSelection === choicePaper && computersMove === choiceScissors)) {
         document.querySelector("h2").innerHTML = `Computer wins! ${computersMove.toUpperCase()} beats ${playerSelection.toUpperCase()} `;
-        document.querySelector(".p2-score").textContent = playerTwoScore++;
+        document.querySelector(".p2-score").textContent = (playerTwoScore++) + 1;
     } else if (playerSelection === computersMove) {
         document.querySelector("h2").innerHTML = `Draw Game! ${playerSelection.toUpperCase()} vs ${computersMove.toUpperCase()} `;
 }
+winnerOutcome();
 }
 
+//targets button content to give clickable feature
 var allButtons = document.querySelectorAll(".content");
 allButtons.forEach(buttons => {
     buttons.addEventListener("click", getPlayerChoice); 
@@ -41,11 +45,13 @@ function getPlayerChoice(event){
     playRound(playerSelection);
 }
 
-// function game() {
-//     var resultsArray = [];
-//     for (let i = 0; i < 5; i++) {
-//         var result = playRound();
-//         resultsArray.push(result);
-//         console.log(result);
-//     }
-//     console.log(resultsArray);}
+function winnerOutcome() {
+    let p1Score = playerOneScore;
+    let p2Score = playerTwoScore;
+    if (p1Score === 5) {
+        document.querySelector(".final-winner").textContent = "Congratulations! You beat the computer!";
+    }else if (p2Score === 5) {
+        document.querySelector(".final-winner").textContent = "The computer has won! Resistance is futile!";
+    } 
+}
+
